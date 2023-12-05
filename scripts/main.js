31b0cb8fa7046e6e3005b6049f1238df2a7470ca
Igor Coelho <xh4rdzn@gmail.com>
2023-12-05 19:30:28 -0300

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digite seu nome: ');
+console.log('Olá, ' + nome);
+let num1 = parseFloat(prompt('Digite um número qualquer: '));
+const num2 = 10;
+console.log(`A soma de ${num1} e ${num2} é ${num1 + num2}`);
+alert('Olhe o console para obter os resultados.');
