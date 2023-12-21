6cbbfa4ac052df3609baf9a1420c8e152ad99fff
Igor Coelho <xh4rdzn@gmail.com>
2023-12-21 17:26:09 -0300

    Finalizando o Desafio de Ciclos

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,29 @@
-function entradaPeso() {
-  const peso = Number(prompt('Digite seu peso em KG(75): '));
-  return peso;
-}
+let opcao = '';
+do {
+  opcao = parseInt(
+    prompt(
+      'Escolha uma das opções abaixo:\n1 - Tabuada de um número qualquer\n2 - Sair',
+    ),
+  );
 
-function entradaAltura() {
-  const altura = Number(prompt('Digite sua altura em metros(1.83):'));
-  return altura;
-}
-
-const calculaIMC = (peso, altura) => {
-  const imc = peso / (altura * altura);
-  return imc.toFixed(2);
-};
-
-const exibeResultado = (imc) => console.log(`Seu imc é de: ${imc}`);
-
-exibeResultado(calculaIMC(entradaPeso(), entradaAltura()));
+  switch (opcao) {
+    case 1:
+      let num = parseFloat(
+        prompt('Digite um número para ter sua tabuada até x10: '),
+      );
+      if (num === 0) {
+        alert('A tabuada do 0 todos os valores são iguais a 0');
+        continue;
+      }
+      for (let i = 0; i <= 10; i++) {
+        alert(`${num} x ${i} = ${num * i}`);
+      }
+      break;
+    case 2:
+      alert('Encerrando o programa...');
+      break;
+    default:
+      alert('Opção inválida. Escolha novamente.');
+      break;
+  }
+} while (opcao !== 2);
